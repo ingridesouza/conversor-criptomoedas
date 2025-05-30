c34c0e1f2ab15c455de7639845bfe391071fc17b
Ingride Souza <134303148+ingridesouza@users.noreply.github.com>
2025-05-30 13:54:57 -0300

    Revert "Add React frontend skeleton"

--- frontend/src/components/Input.jsx
+++
@@ -1,8 +0,0 @@
-export default function Input({ className = '', ...props }) {
-  return (
-    <input
-      className={`w-full px-3 py-2 border rounded focus:outline-none focus:ring ${className}`}
-      {...props}
-    />
-  );
-}
